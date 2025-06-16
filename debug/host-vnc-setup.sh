95f89339df0a0be28a060b88ce73527c545000f8
louis-kiyus <75559366+louis-kiyus@users.noreply.github.com>
2025-06-16 09:27:40 -0700

    migrated to tigervnc

--- debug/host-vnc-setup.sh
+++ debug/host-vnc-setup.sh
@@ -28,33 +28,4 @@ expect eof
 EOF
 
 # now vncserver won't prompt for a password
-vncserver :1
-
-# Write the xstartup content
-cat > "$XSTARTUP" << 'EOF'
-# /bin/sh
-
-export XKL_XMODMAP_DISABLE=1
-unset SESSION_MANAGER
-unset DBUS_SESSION_BUS_ADDRESS
-
-[ -x /etc/vnc/xstartup  ] && exec /etc/vnc/xstartup
-[ -r $HOME/ .Xresources ] && xrdb $HOME/ .Xresources
-xsetroot -solid grey
-
-vncconfig -iconic &
-gnome-panel &
-gnome-settings-daemon &
-metacity &
-nautilus &
-gnome-terminal &
-EOF
-
-# Make xstartup executable
-chmod +x "$XSTARTUP"
-
-echo "✅ xstartup file created at $XSTARTUP"
-
-# Restart VNC server to apply changes
-vncserver -kill :1 
-vncserver :1
+vncserver :1 -localhost no -geometry 1280x800 -depth 24 -SecurityTypes VncAuth
