eb5c8f89cc0d967d46b48c71e773ba539d8c3543
Louis Tan <louistan123@hotmail.my>
2025-06-27 15:12:09 -0400

    bug fix llvm_exp2_f32 DuplicatedNameError (redefinition)
    occurs when there's multiple use of llvm_exp2_f32

--- src/s2lir/intrinsics.py
+++ src/s2lir/intrinsics.py
@@ -2,6 +2,12 @@ from llvmlite import ir
 
 def llvm_exp2_f32(module):
     # https://llvm.org/docs/LangRef.html#llvm-exp2-intrinsic
+    
+    # Check if the function already exists
+    existing_fn = module.globals.get("llvm.exp2.f32", None)
+    if existing_fn is not None:
+        return existing_fn
+    
     return ir.Function(
                 module,
                 ir.FunctionType(ir.FloatType(), [ir.FloatType()]),
